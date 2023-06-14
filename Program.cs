92ed429e016d5491f6e26fdb0ab9aa205679b393
omarmoralesutez <moralesdemetrioomar@gmail.com>
2023-06-13 20:32:28 -0600

    feat: update class Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IAnimal animal1 = new Dog();
+        IAnimal animal2 = new Cat();
+
+        animal1.MakeSound(); // Output: Woof!
+        animal2.MakeSound(); // Output: Meow!
+    }
+}
